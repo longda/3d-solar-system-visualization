c5743873bd3eb564ff0f13852516a65607506d3d
David Long <longda@gmail.com>
2025-04-14 17:36:37 -0400

    chore: planet components

--- TASKS.md
+++ TASKS.md
@@ -36,7 +36,7 @@
   // 🤖 "Add Mercury with 0.4x Earth size, 5.8 AU distance, 88-day orbit"
   ```
 - [x] Create planet configuration file `data/planets.ts` (🤖 "Generate TS planet data with scaled sizes/orbits")
-- [ ] Add remaining planets using config (🤖 "Map planet data to Planet components")
+- [x] Add remaining planets using config (🤖 "Map planet data to Planet components")
 - [ ] Add axial rotations (🤖 "Add planet rotation using useFrame")
 
 ---
--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -4,12 +4,15 @@ import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera, Stars } from '@react-three/drei'
 import * as THREE from 'three'
 import { Planet } from './Planet'
+import { planets } from '../../data/planets' // Import planets data
+
+const EARTH_ORBITAL_PERIOD = 365.25;
 
 export function Scene() {
   return (
     <Canvas style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'black' }}>
       <ambientLight intensity={0.5} />
-      <PerspectiveCamera makeDefault position={[0, 20, 50]} fov={75} />
+      <PerspectiveCamera makeDefault position={[0, 50, 150]} fov={75} /> {/* Adjusted camera further out */}
       <Stars radius={300} depth={50} count={10000} factor={5} saturation={0} fade speed={1} />
       {/* Sun */}
       <mesh>
@@ -17,15 +20,18 @@ export function Scene() {
         <meshStandardMaterial emissive="yellow" emissiveIntensity={2} />
         <pointLight intensity={200} color="yellow" distance={1000} />
       </mesh>
-      {/* Mercury */}
-      <Planet 
-        name="Mercury" 
-        size={0.4} 
-        color="gray" 
-        orbitalRadius={5.8}
-        orbitalSpeed={4.15}
-      />
-      {/* Add other planets here */}
+      
+      {/* Planets */}
+      {planets.map((planet) => (
+        <Planet
+          key={planet.name}
+          name={planet.name}
+          size={planet.size}
+          color={planet.color}
+          orbitalRadius={planet.orbitalRadius}
+          orbitalSpeed={EARTH_ORBITAL_PERIOD / planet.orbitalPeriod} // Calculate relative speed
+        />
+      ))}
     </Canvas>
   )
 } 
